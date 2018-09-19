877f18c24ab0d6c6dad1bc59d74e7627eec4f233
gutsout <eratut@mail.ru>
2018-09-19 21:45:56 +0300

    Update test

--- tests/TestLoki.php
+++ tests/TestLoki.php
@@ -5,18 +5,14 @@ use PHPUnit\Framework\TestCase;
 class TestLoki extends TestCase
 {
 
-    public function testLockFile()
+    public function testCantWriteBeforeReadLock()
     {
         $loki = new Loki('file_to_lock');
         $thorBrother = new Loki('file_to_lock');
 
         $this->assertTrue($loki->readerLock());
         $this->assertFalse($thorBrother->writerLock());
-
-        $this->assertTrue($thorBrother->readerLock());
-        $this->assertFalse($loki->writerLock());
-
-        $this->assertTrue($loki->unlock());
+        $loki->unlock();
         $this->assertTrue($thorBrother->writerLock());
     }
 
